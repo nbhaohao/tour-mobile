da35866d63934e890c7a3c8e616e5de03fde98dc
nbhaohao <nbhaohao@163.com>
2021-03-27 12:45:41 +0800

    feat:完成 fake 支付流程

--- src/pages/order/components/Item/index.tsx
+++ src/pages/order/components/Item/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Order, OrderType } from '@/types/order';
 import timer from '@/utils/timer';
-import { Button } from 'antd-mobile';
+import { Button, Toast } from 'antd-mobile';
 import { House } from '@/types/house';
+import { Http } from '@/utils/http';
 
 interface ItemProps {
   type: OrderType;
@@ -13,11 +14,24 @@ interface ItemProps {
 const Item: React.FC<ItemProps> = ({ type, house, order }) => {
   const { imgs, name, price } = house;
   const { create_time } = order;
+  const handlePay = async () => {
+    const result = await Http({
+      url: '/orders/pay',
+      body: {
+        id: order.id,
+      },
+    });
+    if (result) {
+      Toast.success('支付成功');
+      window.location.reload();
+      return;
+    }
+  };
   const renderPay = () => {
     switch (type) {
       case OrderType.NEED_PAY:
         return (
-          <Button type="warning" size="small">
+          <Button type="warning" size="small" onClick={handlePay}>
             去支付
           </Button>
         );
